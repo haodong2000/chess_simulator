8c4dd0faaaa206c96c9f216eb53e8b5eb624ef59
LeBronLiHD <lebronlihd@outlook.com>
2021-06-05 20:13:02 +0800

    evaluate elephant part done!

--- all_chesses/Elephant.cpp
+++ all_chesses/Elephant.cpp
@@ -35,11 +35,56 @@ Elephant::Elephant(int x, int y, QString name, bool camp, int number, bool isAli
 double Elephant::space_value() {
     if(!isAlive()) return 0;
     if(!canChessMove()) return 0;
-    return 0;
+    double totalValue = 0;
+    const double mid_pos = 2.0;
+    const double left_pos = 1.5;
+    const double right_pos = 1.0;
+    const double up_down_pos = 0.5;
+    QString chessNameSimple = GlobalEnvirIn::Instance()->__QString2SimpleName(chessName());
+    int chessNum = GlobalEnvirIn::Instance()->__QStr2intName(chessNameSimple);
+    if(GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosX() == ElephantPos::mid_x &&
+            GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosY() == ElephantPos::mid_y) {
+        // there are four possible steps
+        if(canElephantMove(ElephantPos::mid, ElephantPos::left_up)) totalValue += mid_pos;
+        if(canElephantMove(ElephantPos::mid, ElephantPos::left_down)) totalValue += mid_pos;
+        if(canElephantMove(ElephantPos::mid, ElephantPos::right_up)) totalValue += mid_pos;
+        if(canElephantMove(ElephantPos::mid, ElephantPos::right_down)) totalValue += mid_pos;
+        return totalValue/(4.0 * mid_pos);
+    }
+    if(GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosX() == ElephantPos::up_up_x &&
+            GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosY() == ElephantPos::up_up_y) {
+        if(canElephantMove(ElephantPos::up_up, ElephantPos::left_up)) totalValue += up_down_pos;
+        if(canElephantMove(ElephantPos::up_up, ElephantPos::right_up)) totalValue += up_down_pos;
+    }
+    if(GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosX() == ElephantPos::down_down_x &&
+            GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosY() == ElephantPos::down_down_y) {
+        if(canElephantMove(ElephantPos::down_down, ElephantPos::left_down)) totalValue += up_down_pos;
+        if(canElephantMove(ElephantPos::down_down, ElephantPos::right_down)) totalValue += up_down_pos;
+    }
+    if(GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosX() == ElephantPos::left_up_x &&
+            GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosY() == ElephantPos::left_up_y) {
+        if(canElephantMove(ElephantPos::left_up, ElephantPos::up_up)) totalValue += left_pos;
+        if(canElephantMove(ElephantPos::left_up, ElephantPos::mid)) totalValue += left_pos;
+    }
+    if(GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosX() == ElephantPos::left_down_x &&
+            GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosY() == ElephantPos::left_down_y) {
+        if(canElephantMove(ElephantPos::left_down, ElephantPos::mid)) totalValue += left_pos;
+        if(canElephantMove(ElephantPos::left_down, ElephantPos::down_down)) totalValue += left_pos;
+    }
+    if(GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosX() == ElephantPos::right_up_x &&
+            GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosY() == ElephantPos::right_up_y) {
+        if(canElephantMove(ElephantPos::right_up, ElephantPos::up_up)) totalValue += right_pos;
+        if(canElephantMove(ElephantPos::right_up, ElephantPos::mid)) totalValue += right_pos;
+    }
+    if(GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosX() == ElephantPos::right_down_x &&
+            GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosY() == ElephantPos::right_down_y) {
+        if(canElephantMove(ElephantPos::right_down, ElephantPos::mid)) totalValue += right_pos;
+        if(canElephantMove(ElephantPos::right_down, ElephantPos::down_down)) totalValue += right_pos;
+    }
+    return totalValue/(2.0 * mid_pos);
 }
 
 bool Elephant::canChessMove() {
-    // if chess in middle point
     QString chessNameSimple = GlobalEnvirIn::Instance()->__QString2SimpleName(chessName());
     int chessNum = GlobalEnvirIn::Instance()->__QStr2intName(chessNameSimple);
     if(GlobalEnvirIn::Instance()->__QStrOrInt2Chess(chessNum, chessNumber())->getPosX() == ElephantPos::mid_x &&
