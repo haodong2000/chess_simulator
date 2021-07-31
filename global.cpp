3bc6432ca53527bd6862b572f95afc0993199c0a
LeBronLiHD <lebronlihd@outlook.com>
2021-07-31 10:41:51 +0800

    9999999 -> 999999999

--- global.cpp
+++ global.cpp
@@ -987,8 +987,8 @@ void GlobalEnvironment::__refershPosValueUnit() {
 }
 
 int GlobalEnvironment::__BoardEvaluate() {
-    if(Ab_gen_1->isAlive() == false) return -9999999;
-    if(Ar_gen_1->isAlive() == false) return 9999999;
+    if(Ab_gen_1->isAlive() == false) return -999999999;
+    if(Ar_gen_1->isAlive() == false) return 999999999;
     // One is the fixed piece strength value;
     // the other is the position value of the piece;
     // the third is the flexibility and cooperation value of the piece;
