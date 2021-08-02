c5686e25389269bd2d96d0c38c7d87d4ea066708
LeBronLiHD <lebronlihd@outlook.com>
2021-08-03 00:10:38 +0800

    StrategyMode < 0 || > 3 is inValid

--- init_endgame.cpp
+++ init_endgame.cpp
@@ -175,7 +175,7 @@ void init_endgame::setInitVisionBoard() {
 
 void init_endgame::setInitStrategyBoard(int strategy_mode) {
     // 2021-08-01
-    if(strategy_mode <= 0 || strategy_mode > PARAM::EndGame::MaxStrategyModeNum) {
+    if(strategy_mode < 0 || strategy_mode > PARAM::EndGame::MaxStrategyModeNum) {
         qDebug() << "ERROR: init_endgame.cpp function:setInitStrategyBoard() line:179 strategy_mode inValid!";
         return;
     }
